ac860436d9d2d6ca58f77911a4fddfa9caf7dede
djacquin <danielcamilo853@gmail.com>
2024-05-08 17:16:25 -0500

    package.json is updated

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const envs = {
     port: envVar.get('PORT').asString(),
     type: envVar.get('TYPE').default('postgres'),
-    host: envVar.get('LOCALHOST').asString(),
+    host: envVar.get('HOST').asString(),
     db_port: envVar.get('DB_PORT').asPortNumber(),
     username: envVar.get('USER_NAME').asString(),
     password: envVar.get('PASSWORD').asString(),
